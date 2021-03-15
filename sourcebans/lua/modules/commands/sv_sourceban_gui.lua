3ea6313fdd58b61b22bd7e61ee498a2fac605497
How is this not taken <alexdunford09@gmail.com>
2021-03-15 15:04:41 -0700

    Update sv_sourceban_gui.lua

--- sourcebans/lua/modules/commands/sv_sourceban_gui.lua
+++ sourcebans/lua/modules/commands/sv_sourceban_gui.lua
@@ -1,40 +1,32 @@
 MsgC(Color(235, 245, 0), "Loading Server side functionality of SourceBans\n")
+util.AddNetworkString("sm_BanPlayer")
+    net.Receive("sm_BanPlayer", function ( len, Player )
+        if Player:IsAdmin() then
+            local target = net.ReadEntity()
+            local time = net.ReadUInt(32)
+            local reason = net.ReadString()
 
-    local function denyifnotstaff(Player)
-        if not Player:IsAdmin() then
-            sourcebans_notify(Player, "You must be a staff member to do this!", 1, 4)
-        return end
-    end
+            if reason == '' then
+                sourcebans_notify(Player, "You must have a reason!", 1, 4)
+            return end
 
-    util.AddNetworkString("sm_BanPlayer")
-        net.Receive("sm_BanPlayer", function ( len, Player )
-                denyifnotstaff(Player) 
+            if !target:IsPlayer() and !IsValid(target) then
+                sourcebans_notify(Player, "Not a valid player!", 0, 4)
+            return end
 
-                local target = net.ReadEntity()
-                local time = net.ReadUInt(32)
-                local reason = net.ReadString()
+            if !tonumber(time) then
+                sourcebans_notify(Player, "Ban length must be a number!", 1, 4)
+            return end
 
-                if reason == '' then
-                    sourcebans_notify(Player, "You must have a reason!", 1, 4)
-                return end
+            sourcebans.BanPlayer(target, time*60, reason, Player, callback)
+            sourcebans_notify(Player, "Banned: " .. target:Nick(), 0, 4)
+            PrintMessage(3, target:Nick() .. " was banned for: " .. reason)
+        end
+    end)
 
-                if !target:IsPlayer() and !IsValid(target) then
-                    sourcebans_notify(Player, "Not a valid player!", 0, 4)
-                return end
-
-                if !tonumber(time) then
-                    sourcebans_notify(Player, "Ban length must be a number!", 1, 4)
-                return end
-
-                sourcebans.BanPlayer(target, time*60, reason, Player, callback)
-                sourcebans_notify(Player, "Banned: " .. target:Nick(), 0, 4)
-                PrintMessage(3, target:Nick() .. " was banned for: " .. reason)
-        end)
-
-    util.AddNetworkString("sm_AddBan")
-        net.Receive("sm_AddBan", function( len, Player )
-            denyifnotstaff(Player)
-
+util.AddNetworkString("sm_AddBan")
+    net.Receive("sm_AddBan", function( len, Player )
+        if Player:IsAdmin() then
             local steamid = net.ReadString()
             local time = net.ReadUInt(32)
             local reason = net.ReadString()
@@ -56,12 +48,12 @@ MsgC(Color(235, 245, 0), "Loading Server side functionality of SourceBans\n")
 
             sourcebans.BanPlayerBySteamID(steamid, time*60, reason, Player, name, callback)
             sourcebans_notify(Player, "Banned: " .. steamid .. " (" .. name .. ")", 0, 4)
-        end)
+        end
+    end)
 
-    util.AddNetworkString("sm_Unban")
-        net.Receive("sm_Unban", function( len, Player )
-            denyifnotstaff(Player) 
-
+util.AddNetworkString("sm_Unban")
+    net.Receive("sm_Unban", function( len, Player )
+        if Player:IsAdmin() then
             local steamid = net.ReadString()
             local reason = net.ReadString()
 
@@ -75,12 +67,12 @@ MsgC(Color(235, 245, 0), "Loading Server side functionality of SourceBans\n")
 
             sourcebans.UnbanPlayerBySteamID(steamid, reason, Player)
             sourcebans_notify(Player, "Unbanned: " .. steamid, 0, 4)
-        end)
+        end
+    end)
 
-    util.AddNetworkString("sm_KickPlayer")
-        net.Receive("sm_KickPlayer", function ( len, Player )
-            denyifnotstaff(Player)
-
+util.AddNetworkString("sm_KickPlayer")
+    net.Receive("sm_KickPlayer", function ( len, Player )
+        if Player:IsAdmin() then
             local target = net.ReadEntity()
             local reason = net.ReadString()
 
@@ -97,14 +89,15 @@ MsgC(Color(235, 245, 0), "Loading Server side functionality of SourceBans\n")
             else
                 target:Kick("You were kicked for: " .. reason .. " \nAdmin: " .. Player:Nick())
             end
+
             PrintMessage(3, target:Nick() .. " was kicked for: " .. reason)
             sourcebans_notify(Player, "Kicked: " .. target:Nick(), 0, 4)
-        end)
+        end
+    end)
 
-    util.AddNetworkString("sm_FreezePlayer")
-        net.Receive("sm_FreezePlayer", function ( len, Player )
-            denyifnotstaff(Player)
-
+util.AddNetworkString("sm_FreezePlayer")
+    net.Receive("sm_FreezePlayer", function ( len, Player )
+        if Player:IsAdmin() then
             local target = net.ReadEntity()
 
             if !target:IsPlayer() and !IsValid(target) then
@@ -113,16 +106,19 @@ MsgC(Color(235, 245, 0), "Loading Server side functionality of SourceBans\n")
 
             if !target:IsFrozen() then
                 target:Freeze(true)
+                target:GodEnable()
                 sourcebans_notify(Player, "Froze: " .. target:Nick(), 0, 4)
             else
                 target:Freeze(false)
+                target:GodDisable()
                 sourcebans_notify(Player, "Unfroze: " .. target:Nick(), 0, 4)
             end
-        end)
+        end
+    end)
 
-    util.AddNetworkString("sm_ChangeTeam")
-        net.Receive("sm_ChangeTeam", function( len, Player )
-            denyifnotstaff(Player)
+util.AddNetworkString("sm_ChangeTeam")
+    net.Receive("sm_ChangeTeam", function( len, Player )
+        if Player:IsAdmin() then
 
             local target = net.ReadEntity()
             local teamid = net.ReadUInt(32)
@@ -137,12 +133,12 @@ MsgC(Color(235, 245, 0), "Loading Server side functionality of SourceBans\n")
 
             target:changeTeam(teamid, true, true)
             sourcebans_notify(Player, "Set Team: " .. target:Nick(), 0, 4)
-        end)
+        end
+    end)
 
-    util.AddNetworkString("sm_RespawnPlayer")
-        net.Receive("sm_RespawnPlayer", function ( len, Player )
-            denyifnotstaff(Player)
-
+util.AddNetworkString("sm_RespawnPlayer")
+    net.Receive("sm_RespawnPlayer", function ( len, Player )
+        if Player:IsAdmin() then
             local target = net.ReadEntity()
 
             if !target:IsPlayer() and !IsValid(target) then
@@ -155,12 +151,12 @@ MsgC(Color(235, 245, 0), "Loading Server side functionality of SourceBans\n")
 	        target:DrawWorldModel(true)
             target:Spawn();
             sourcebans_notify(Player, "Respawned: " .. target:Nick(), 0, 4)
-        end)
+        end
+    end)
 
-    util.AddNetworkString("sm_KillPlayer")
-        net.Receive("sm_KillPlayer", function ( len, Player )
-            denyifnotstaff(Player)
-
+util.AddNetworkString("sm_KillPlayer")
+    net.Receive("sm_KillPlayer", function ( len, Player )
+        if Player:IsSuperAdmin() then
             local target = net.ReadEntity()
 
             if !target:IsPlayer() and !IsValid(target) then
@@ -173,12 +169,12 @@ MsgC(Color(235, 245, 0), "Loading Server side functionality of SourceBans\n")
 	        target:DrawWorldModel(true)
             target:Kill()
             sourcebans_notify(Player, "Killed: " .. target:Nick(), 0, 4)
-        end)
+        end
+    end)
 
-    util.AddNetworkString("sm_Godmode")
-        net.Receive("sm_Godmode", function ( len, Player )
-            denyifnotstaff(Player)
-
+util.AddNetworkString("sm_Godmode")
+    net.Receive("sm_Godmode", function ( len, Player )
+        if Player:IsSuperAdmin() then
             local target = net.ReadEntity()
 
             if !target:IsPlayer() and !IsValid(target) then
@@ -192,12 +188,12 @@ MsgC(Color(235, 245, 0), "Loading Server side functionality of SourceBans\n")
                 target:GodDisable()
                 sourcebans_notify(Player, "Disabled god mode for: " .. target:Nick(), 0, 4)
             end
-        end)
+        end
+    end)
 
-    util.AddNetworkString("sm_GotoPlayer")
-        net.Receive("sm_GotoPlayer", function ( len, Player )
-            denyifnotstaff(Player)
-
+util.AddNetworkString("sm_GotoPlayer")
+    net.Receive("sm_GotoPlayer", function ( len, Player )
+        if Player:IsAdmin() then
             local target = net.ReadEntity()
 
             if Player:InVehicle() then
@@ -210,13 +206,13 @@ MsgC(Color(235, 245, 0), "Loading Server side functionality of SourceBans\n")
 
             Player:SetPos( target:GetPos() + target:GetForward() * 50 )
             sourcebans_notify(Player, "Teleported to: " .. target:Nick(), 0, 4)
-        end)
+        end
+    end)
 
     
-    util.AddNetworkString("sm_BringPlayer")
-        net.Receive("sm_BringPlayer", function ( len, Player )
-            denyifnotstaff(Player)
-
+util.AddNetworkString("sm_BringPlayer")
+    net.Receive("sm_BringPlayer", function ( len, Player )
+        if Player:IsAdmin() then
             local target = net.ReadEntity()
 
             if target:InVehicle() then
@@ -232,13 +228,13 @@ MsgC(Color(235, 245, 0), "Loading Server side functionality of SourceBans\n")
             return end
 
             target:SetPos( Player:GetPos() + Player:GetForward() * 50 )
-	    sourcebans_notify(Player, "Brought: " .. target:Nick(), 0, 4)
-        end)
+		    sourcebans_notify(Player, "Brought: " .. target:Nick(), 0, 4)
+        end
+    end)
     
-    util.AddNetworkString("sm_StripPlayer")
-        net.Receive("sm_StripPlayer", function( len, Player )
-            denyifnotstaff(Player)
-
+util.AddNetworkString("sm_StripPlayer")
+    net.Receive("sm_StripPlayer", function( len, Player )
+        if Player:IsAdmin() then
             local target = net.ReadEntity()
 
             if !target:IsPlayer() then 
@@ -247,20 +243,30 @@ MsgC(Color(235, 245, 0), "Loading Server side functionality of SourceBans\n")
 
             target:StripWeapons()
             sourcebans_notify(Player, "Stripped weapons from: " .. target:Nick(), 0, 4)
-        end)
+        end
+    end)
+util.AddNetworkString("sm_GetIP")
+    net.Receive("sm_GetIP", function(len, Player)
+        if Player:IsSuperAdmin() then
+            local target = net.ReadEntity()
 
-    --[[ MENU NETWORKING BELOW]]--
-    util.AddNetworkString("sm_AdminMenu")
-    concommand.Add("sm_adminmenu", function( Player, cmd )
-        denyifnotstaff(Player)
+            if !target:IsPlayer() then
+                sourcebans_notify(Player, "Not a valid player!", 1, 4)
+            return end
 
-        net.Start("sm_AdminMenu")
-            net.WriteEntity(Player)
-        net.Send(Player)
+            net.Start("sm_GetIP")
+                net.WriteString(sourcebans.getIP(target))
+            net.Send(Player)
+        end
     end)
 
-    --[[Admin Refresh every 30secs]]--
-    local function adminrefresh()
-        RunConsoleCommand("sm_rehash")
-    end
-    timer.Create("Refresh Admin", 30, 0, function() adminrefresh() end)
+    --[[ MENU NETWORKING BELOW]]--
+util.AddNetworkString("sm_AdminMenu")
+    concommand.Add("sm_adminmenu", function( Player, cmd )
+        if Player:IsAdmin() then
+            net.Start("sm_AdminMenu")
+                net.WriteEntity(Player)
+            net.Send(Player)
+        end
+    end)
+timer.Create("Refresh Admin", 30, 0, function() RunConsoleCommand("sm_rehash") end)

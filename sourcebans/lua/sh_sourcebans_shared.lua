677a799963a7aee11400f9dfac13b2e6815e5e36
How is this not taken <alexdunford09@gmail.com>
2021-03-26 21:01:44 -0700

    Update sh_sourcebans_shared.lua

--- sourcebans/lua/sh_sourcebans_shared.lua
+++ sourcebans/lua/sh_sourcebans_shared.lua
@@ -1,6 +1,28 @@
 // Keeps track of ban history. Set true to record kicks, false to kick w/o record. Recommendation: false to save database space.
 sourcebans_kickHistory = false
 
+//SourceBans Flags. B is given to most functions as those are basic admin privileges. You can use kick and ban for something else or add as an extra check.
+sourcebans_FLAG_RESERVED = "a"
+sourcebans_FLAG_GENERIC = "b"
+sourcebans_FLAG_KICK = "c"
+sourcebans_FLAG_BAN = "d"
+sourcebans_FLAG_UNBAN = "e"
+sourcebans_FLAG_SLAY = "f"
+sourcebans_FLAG_MAP = "g"
+sourcebans_FLAG_CVARS = "h"
+sourcebans_FLAG_EXECCONFIG = "i"
+sourcebans_FLAG_ACHAT = "j"
+sourcebans_FLAG_VOTES = "k"
+sourcebans_FLAG_PASSW = "l"
+sourcebans_FLAG_RCON = "m"
+sourcebans_FLAG_CHEATS = "n"
+sourcebans_FLAG_CUSTOM_O = "o"
+sourcebans_FLAG_CUSTOM_P = "p"
+sourcebans_FLAG_CUSTOM_Q = "q"
+sourcebans_FLAG_CUSTOM_R = "r"
+sourcebans_FLAG_CUSTOM_S = "s"
+sourcebans_FLAG_CUSTOM_T = "t"
+
 //Taken from DarkRP
 function sm_FindPlayer(info)
 	    if not info or info == "" then return nil end

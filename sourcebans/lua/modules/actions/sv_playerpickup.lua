8230abcbfb7492e1fd1f09e4becade150667d509
How is this not taken <alexdunford09@gmail.com>
2021-10-28 20:37:12 -0700

    Merged functions w/ hooks

--- sourcebans/lua/modules/actions/sv_playerpickup.lua
+++ sourcebans/lua/modules/actions/sv_playerpickup.lua
@@ -1,17 +1,15 @@
-local function AdminPhysgunPickup( ply, ent )
+hook.Add( "PhysgunPickup", "AdminPhysgunPickup", function(ply, ent)
 	if ply:IsAdmin() then
 		if not IsValid(ent) or not ent:IsPlayer() then return end
 			ent:SetMoveType(MOVETYPE_NONE)
 			ent:Freeze(true)
 		return true
 	end
-end
-hook.Add( "PhysgunPickup", "AdminPhysgunPickup", AdminPhysgunPickup )
+end)
 	
-local function AdminPhysgunDrop( ply, ent )
+hook.Add( "PhysgunDrop", "AdminPhysgunDrop", function(ply, ent)
 	if not IsValid(ent) or not ent:IsPlayer() then return end
 		ent:SetMoveType(MOVETYPE_WALK)
 		ent:Freeze(false)
 	return false
-end
-hook.Add( "PhysgunDrop", "AdminPhysgunDrop", AdminPhysgunDrop)
+end)

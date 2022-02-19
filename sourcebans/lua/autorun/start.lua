cd7a58c0bf11e8d08b1bded76c9b4b88c37ff23f
How is this not taken <alexdunford09@gmail.com>
2022-02-19 12:24:26 -0800

    Delete sourcebans/lua directory

--- sourcebans/lua/autorun/start.lua
+++
@@ -1,5 +0,0 @@
-if (SERVER) then
-	include("sv_sourcebans_main.lua")
-elseif (CLIENT) then
-	include("cl_sourcebans_main.lua")
-end
--- sourcebans/lua/modules/notifications/cl_sourcebans_notifcations.lua
+++
@@ -1,9 +0,0 @@
-net.Receive("sourcebans_notification", function()
-	local Text = net.ReadString()
-	local Type = net.ReadInt(4)
-	local Time = net.ReadInt(8)
-
-	print(Text);
-	notification.AddLegacy(Text, Type, Time);
-	surface.PlaySound( "buttons/button15.wav" )
-end)
